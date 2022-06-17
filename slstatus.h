0b81087305c154f750d18c267f316334365ecaf2
luis-castaneda7 <luis_casta7@outlook.com>
2022-06-17 16:56:32 -0500

    added pomo

--- slstatus.h
+++ slstatus.h
@@ -82,3 +82,6 @@ const char *vol_perc(const char *card);
 /* wifi */
 const char *wifi_perc(const char *interface);
 const char *wifi_essid(const char *interface);
+
+/* pomo */
+const char *pomo(void);
