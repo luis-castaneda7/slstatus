2d23f08f197c633da19a9294184764eee7123b56
luis-castaneda7 <luis_casta7@outlook.com>
2022-07-05 20:34:08 -0500

    modify status

--- config.h
+++ config.h
@@ -66,8 +66,8 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ battery_perc, "%s%% ",           "BAT1"  },
-	{ pomo,     	"%s ",             NULL  },
-	{ datetime, 	"%s ",           "%a-%d" },
-	{ datetime, 	"%s",           "%I:%M"  },
+	{ battery_perc, "[%s%%] ",           "BAT1"  },
+	{ pomo,     	"[%s] ",             NULL  },
+	{ datetime, 	"[%s] ",           "%a-%d" },
+	{ datetime, 	"[%s]",           "%I:%M"  },
 };
